7e2128554a5dfb66f2539119ffac7e92220fd7ab
priya9999 <36176490+priya9999@users.noreply.github.com>
2019-12-03 12:26:19 -0800

    displaying todo list with angular

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "src/app/models/Todo";
+
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: "one",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Two",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Three",
+        completed: false
+      }
+    ];
+  }
+}
